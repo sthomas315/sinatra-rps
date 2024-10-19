01e3f8d7bdcdd3d0f352c87bcababc37f817194d
sthomas315 <147417972+sthomas315@users.noreply.github.com>
2024-10-19 23:53:03 +0000

    second

--- app.rb
+++ app.rb
@@ -2,32 +2,22 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+ erb(:hstyle)
 end
 
 get("/rock") do
 
   plays = ["rock", "paper", "scissors"]
-  comp_move = plays.sample
+  @comp_move = plays.sample
 
-  if comp_move == "rock"
-      outcome == "tied"
-  elsif comp_move == "paper"
-      outcome_2 = "lost"
-  elsif comp_move == "scissors"
-      outcome_3 = "won"
+  if @comp_move == "rock"
+      @outcome = "tied"
+  elsif @comp_move == "paper"
+      @outcome = "lost"
+  elsif @comp_move == "scissors"
+      @outcome = "won"
   end
-
-  "
-  <h2> We played rock! </h2>
-
-  <h2> They played #{comp_move}! </h2>
-
-  <h2> we #{outcome}! </h2>
-  "
+  erb(:rstyle)
 end
 
 get("/paper") do
@@ -43,13 +33,7 @@ get("/paper") do
       outcome_3 = "won"
   end
 
-  "
-  <h2> We played rock! </h2>
-
-  <h2> They played #{comp_move}! </h2>
-
-  <h2> we #{outcome}! </h2>
-  "
+  erb(:rstyle)
 end
 
 
@@ -66,11 +50,5 @@ get("/scissors") do
       outcome_3 = "won"
   end
 
-  "
-  <h2> We played rock! </h2>
-
-  <h2> They played #{comp_move}! </h2>
-
-  <h2> we #{outcome}! </h2>
-  "
+  erb(:rstyle)
 end
