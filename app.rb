9f36667c8b3be41e23057041853cb82f14215bc7
sthomas315 <147417972+sthomas315@users.noreply.github.com>
2024-10-20 00:04:23 +0000

    third

--- app.rb
+++ app.rb
@@ -23,32 +23,29 @@ end
 get("/paper") do
 
   plays = ["rock", "paper", "scissors"]
-  comp_move = plays.sample
+  @comp_move_2 = plays.sample
 
-  if comp_move == "rock"
-      outcome == "tied"
-  elsif comp_move == "paper"
-      outcome_2 = "lost"
-  elsif comp_move == "scissors"
-      outcome_3 = "won"
+  if @comp_move_2 == "rock"
+      @outcome_2 = "won"
+  elsif @comp_move_2 == "paper"
+      @outcome_2 = "tied"
+  elsif @comp_move_2 == "scissors"
+      @outcome_2 = "lost"
   end
-
-  erb(:rstyle)
+  erb(:pstyle)
 end
-
 
 get("/scissors") do
 
   plays = ["rock", "paper", "scissors"]
-  comp_move = plays.sample
+  @comp_move = plays.sample
 
-  if comp_move == "rock"
-      outcome == "tied"
-  elsif comp_move == "paper"
-      outcome_2 = "lost"
-  elsif comp_move == "scissors"
-      outcome_3 = "won"
+  if @comp_move_3 == "rock"
+      @outcome_3 = "lost"
+  elsif @comp_move_3 == "paper"
+      @outcome_3 = "won"
+  elsif @comp_move_3 == "scissors"
+      @outcome_3 = "tied"
   end
-
-  erb(:rstyle)
+  erb(:sstyle)
 end
